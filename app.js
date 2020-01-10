93752c8cdca7df3b7507d7e2add7c3362abf8dbc
vladok089 <vladok.sha089@gmail.com>
2020-01-10 04:53:07 +0200

    onduty function

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
+var fs = require("fs");
+var path = require('path');
+
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = '953388296:AAEV4PUN-xy5hQgTmQfgXpQd2t_LEtJveyc';
 const bot = new TelegramBot(token, {polling: true});
 
-onduty = [278311577, 358092524];
+// var onduty = []; /*278311577, 358092524*/
+if (!path.exists){
+  fs.writeFileSync('./onduty.json', '[]')
+}
+
+
+var onduty = JSON.parse(fs.readFileSync("./onduty.json"));
+
 var app = express();
 
+// app.use('/', express.static(__dirname + '/public0'));
 app.use('/:num', express.static(__dirname + '/public'));
-app.use('/', express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+  let err = new Error(`Error! Table number isn't declared! Go to http://xxx.xxx.xxx.xxx/[table_number]`);
+  res.send(err.message);
+  throw err
+})
 
 app.get('/:num', (req, res) => {
-  res.sendFile('index.html');
+    res.sendFile('./public/index.html');
 })
 
 app.post('/:num/waiter', (req, res) => {
@@ -37,9 +54,31 @@ app.post('/:num/check', (req, res) => {
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Mr-call-app listening on port 3000!');
 });
 
 bot.on('message', (msg) => {
-  console.log(msg.from.id);
+  // console.log(msg.from.id);
 });
+
+bot.on('message', (msg) => {
+  if (msg.text == 'on') {
+    if (onduty.indexOf(msg.from.id) == -1){
+      onduty.push(msg.from.id);
+      jsonText = JSON.stringify(onduty);
+      console.log(jsonText);
+      fs.writeFileSync('./onduty.json', jsonText);
+    }
+    console.log(onduty);
+  }
+
+  if (msg.text == 'off') {
+    while (onduty.indexOf(msg.from.id) != -1) {
+      onduty.splice(onduty.indexOf(msg.from.id), 1);
+    }
+    jsonText = JSON.stringify(onduty);
+    console.log(jsonText);
+    fs.writeFileSync('./onduty.json', jsonText);
+    console.log(onduty);
+  }
+})
